2a5a993008bca0269228dfd7f99c5940633eaaa7
Arthur Dias <arthurdiascorporativo@gmail.com>
2022-04-24 15:18:47 -0300

    feat: authentication

---
+++ server/src/database/index.js
@@ -0,0 +1,8 @@
+//defini uma constante
+const mongoose = require('mongoose');
+//vou conectar ao meu banco de dados
+mongoose.connect('mongodb://MongoDb2:MongoDb22Eu@crud.f5i8p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useMongoClient: true});
+//indicar a classe de promisses que  o mongoose vai utilizar
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
